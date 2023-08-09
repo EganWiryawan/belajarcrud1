5b7d6294f401228cc0ea8ce27758784f38887876
Egan Wiryawan <egantampan123@gmail.com>
2023-08-09 08:38:39 +0700

    Initial commit

---
+++ resources/views/pages/peserta.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.content')
+@section('content')
+<section class="section">
+    <div class="section-header">
+        <h1>Peserta pages</h1>
+    </div>
+    <div class="card">
+    <div class="card-header">
+        <h4>Data peserta Didik</h4>
+
+        </div>
+        <div class="card-body">
+            <p>Tabel</p>
+        </div>
+    </div>
+</section>
+@endsection
